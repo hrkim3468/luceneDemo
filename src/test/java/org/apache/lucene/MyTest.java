f3058fd3765a827aa47ca8ee8e050dc47ee615e4
hrkim3468 <hrkim3468@gmail.com>
2017-01-13 22:13:10 +0900

    Demo 추가

---
+++ src/test/java/org/apache/lucene/MyTest.java
@@ -0,0 +1,19 @@
+package org.apache.lucene;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() throws ClassNotFoundException, SQLException {
+		
+		assertEquals("홍길동", "홍길동");
+	}
+
+	
+	
+}
